5a009f55d47d22e2c6c5569e9eaa0df8e5c9ffc3
mizutee <chrisansenw96@gmail.com>
2024-02-15 17:57:19 +0700

    Done Authentication

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const authentication = async (req, res, next) => {
+  try {
+    if (!req.headers.access_token) {
+      throw { message: "No Authentication", status: 403 };
+    }
+
+    const accessToken = req.headers.access_token;
+
+    const token = accessToken.split(" ")[1];
+
+    const verifyToken = jwt.verify(token, process.env.HASH_SECRET);
+
+    req.user = verifyToken
+
+    next()
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = authentication;
--- server/routers/router.js
+++ server/routers/router.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/config/mongodbConnection");
 const UserController = require("../controllers/userController");
+const authentication = require("../middlewares/authentication");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.get("/", authentication, (req, res) => {
+  console.log(req.user, "<<< ini requser dari router");
+});
 
 module.exports = router;
